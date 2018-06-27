d0bf1ed91872f8e75a762521bef0692d18e05a68
anshanxulizhi <15001295170@163.com>
2018-06-27 14:41:03 +0800

    用户DTO

--- src/main/java/com/xulizhi/demo/dto/UserDTO.java
+++ src/main/java/com/xulizhi/demo/dto/UserDTO.java
@@ -1,8 +1,12 @@
 package com.xulizhi.demo.dto;
 
-public class UserDTO {
+import com.xulizhi.demo.common.BaseDTO;
+
+public class UserDTO extends BaseDTO {
 
     private String name;//用户名
+
+    private String createName;//创建人
 
     public void setName(String name) {
         this.name = name;
@@ -10,5 +14,13 @@ public class UserDTO {
 
     public String getName() {
         return name;
+    }
+
+    public String getCreateName() {
+        return createName;
+    }
+
+    public void setCreateName(String createName) {
+        this.createName = createName;
     }
 }
