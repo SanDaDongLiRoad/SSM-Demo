7863dfbdfb5ceb88619408c9c34d85541fe92727
anshanxulizhi <15001295170@163.com>
2018-07-03 16:39:36 +0800

    分页返回数据工具类

--- src/main/java/com/xulizhi/demo/utils/DataGridResult.java
+++ src/main/java/com/xulizhi/demo/utils/DataGridResult.java
@@ -4,17 +4,39 @@ import java.util.List;
 
 public class DataGridResult {
 
-    private long total;
-    private List<?> rows;
+    private long currentPageNo;//当前页数
+    private long size;//每页显示记录数
+    private long total;//总记录数
+    private List<?> rows;//总记录数据集合
+
+    public long getCurrentPageNo() {
+        return currentPageNo;
+    }
+
+    public void setCurrentPageNo(long currentPageNo) {
+        this.currentPageNo = currentPageNo;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
     public long getTotal() {
         return total;
     }
+
     public void setTotal(long total) {
         this.total = total;
     }
+
     public List<?> getRows() {
         return rows;
     }
+
     public void setRows(List<?> rows) {
         this.rows = rows;
     }
