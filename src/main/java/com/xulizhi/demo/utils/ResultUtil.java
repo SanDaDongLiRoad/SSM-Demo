3bfecc7cfc295ad06f5e847fb4b2eb5097e9814d
anshanxulizhi <15001295170@163.com>
2018-11-13 17:38:03 +0800

    修改代码规范

---
+++ src/main/java/com/xulizhi/demo/utils/ResultUtil.java
@@ -0,0 +1,28 @@
+package com.xulizhi.demo.utils;
+
+import com.xulizhi.demo.domain.Result;
+
+/**
+ * @author lenovo
+ */
+public class ResultUtil {
+
+    public static Result success(Object object){
+        Result result = new Result();
+        result.setCode(0);
+        result.setMsg("操作成功");
+        result.setData(object);
+        return result;
+    }
+
+    public static Result success(){
+        return success(null);
+    }
+
+    public static Result error(Integer code,String msg){
+        Result result = new Result();
+        result.setCode(code);
+        result.setMsg(msg);
+        return result;
+    }
+}
